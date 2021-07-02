b646b5cabcda7118d651d829508fc6195b8b4af8
VictorShlaev <34406319+VictorShlaev@users.noreply.github.com>
2021-07-02 14:10:40 +0300

    Update Insert Interval.cpp

--- Insert Interval.cpp
+++ Insert Interval.cpp
@@ -1,9 +1,8 @@
 /*
 * Given a set of non-overlapping intervals, insert a new interval into the intervals (merge if necessary).
 * You may assume that the intervals were initially sorted according to their start times.
-* Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
-* Output: [[1,6],[8,10],[15,18]]
-* Explanation: Since intervals [1,3] and [2,6] overlaps, merge them into [1,6].
+* Input: intervals = [[1,3],[6,9]], newInterval = [2,5]
+* Output: [[1,5],[6,9]]
 */
 #include <algorithm>
 #include <cmath>
